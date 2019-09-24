50cd4af27d2f65acfcc75943e6156fef739d21aa
zhaoyingli <1830922083@qq.com>
2019-09-24 12:05:33 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { Button, Layout, Icon, Drawer, Radio, Menu } from "ant-design-vue";
-
+import Authorized from "./components/Authorized";
+import Auth from "./directives/auth";
 Vue.config.productionTip = false;
 
 Vue.use(Button);
@@ -12,6 +13,9 @@ Vue.use(Icon);
 Vue.use(Drawer);
 Vue.use(Radio);
 Vue.use(Menu);
+Vue.component("Authorized", Authorized);
+Vue.use(Auth);
+
 new Vue({
   router,
   store,
