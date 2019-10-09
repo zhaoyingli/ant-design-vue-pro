8f2b5a2b9b314a4b48cd9a2977fd04fb0095c58c
赵映历 <zhaoyingli@copote.com>
2019-10-09 17:03:18 +0800

    commit

---
+++ src/store/modules/form.js
@@ -0,0 +1,38 @@
+import router from "../../router.js";
+import request from "../../utils/request.js";
+
+const state = {
+  step: {
+    payCount: "123456"
+  }
+};
+
+const actions = {
+  async submitStepForm({ commit }, { payload }) {
+    await request({
+      url: "/api/form",
+      method: "POST",
+      data: payload
+    });
+    commit("saveStepFormData", payload);
+    router.push("/form/step-from/result");
+  }
+};
+
+const mutations = {
+  saveStepFormData(state, { payload }) {
+    state.step = {
+      ...(state.step = {
+        ...state.step,
+        ...payload
+      })
+    };
+  }
+};
+
+export default {
+  namespaced: true,
+  state,
+  actions,
+  mutations
+};
